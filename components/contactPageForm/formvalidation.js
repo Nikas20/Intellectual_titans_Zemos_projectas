c3f1dc2d8b44d7ccdb5ac9ee5c938a0a1852d7f1
Nikas20 <nikas.simatolskis@stud.techin.lt>
2025-12-05 13:24:11 +0200

    tray into validation

---
+++ components/contactPageForm/formvalidation.js
@@ -0,0 +1,16 @@
+// const form = document.querySelector("#form")
+
+// const inputName = document.querySelector("#nameForm")
+// const inputNameError = document.querySelector("#")
+// const inputEmail = document.querySelector("#")
+// const inputEmailError = document.querySelector("#")
+// const inputCompanyName = document.querySelector("#")
+// const inputCompanyNameError = document.querySelector("#")
+// const inputTitle = document.querySelector("#")
+// const inputTitleError = document.querySelector("#")
+// const inputMessage = document.querySelector("#")
+// const inputMessageError = document.querySelector("#")
+// const inputCheckout = document.querySelector("#")
+// const inputCheckoutError = document.querySelector("#")
+
+//////////////////////////////////////// maybe later do more strong validation /////////////////////////////////////////
