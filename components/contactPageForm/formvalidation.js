3500eb87d908282ae0663d79ff1507da7ea695a8
Nikas20 <nikas.simatolskis@stud.techin.lt>
2025-12-08 08:45:24 +0200

    Add working validation for email

--- components/contactPageForm/formvalidation.js
+++ components/contactPageForm/formvalidation.js
@@ -1,16 +1,28 @@
-// const form = document.querySelector("#form")
+const form = document.querySelector("#form")
 
-// const inputName = document.querySelector("#nameForm")
-// const inputNameError = document.querySelector("#")
-// const inputEmail = document.querySelector("#")
-// const inputEmailError = document.querySelector("#")
-// const inputCompanyName = document.querySelector("#")
-// const inputCompanyNameError = document.querySelector("#")
-// const inputTitle = document.querySelector("#")
-// const inputTitleError = document.querySelector("#")
-// const inputMessage = document.querySelector("#")
-// const inputMessageError = document.querySelector("#")
-// const inputCheckout = document.querySelector("#")
-// const inputCheckoutError = document.querySelector("#")
+const inputName = document.querySelector("#nameForm")
+const inputNameError = document.querySelector("#nameErorForm")
+const inputEmail = document.querySelector("#emailForm")
+const inputEmailError = document.querySelector("#emailErorForm")
+const inputCompanyName = document.querySelector("#companyNameForm")
+const inputCompanyNameError = document.querySelector("#companyNameErorForm")
+const inputTitle = document.querySelector("#titleForm")
+const inputTitleError = document.querySelector("#titleErorForm")
+const inputMessage = document.querySelector("#messageForm")
+const inputMessageError = document.querySelector("#messageErorForm")
+const inputCheckout = document.querySelector("#checkForm")
+const inputCheckoutError = document.querySelector("#checkErorForm")
 
-//////////////////////////////////////// maybe later do more strong validation /////////////////////////////////////////
+
+
+inputNameError.innerHTML = `${inputName.validationMessage}`
+
+inputEmailError.innerHTML = `${inputEmail.validationMessage}`
+
+inputCompanyNameError.innerHTML = `${inputCompanyName.validationMessage}`
+
+inputTitleError.innerHTML = `${inputTitle.validationMessage}`
+
+inputMessageError.innerHTML = `${inputMessage.validationMessage}`
+
+inputCheckoutError.innerHTML = `${inputCheckout.validationMessage}`
